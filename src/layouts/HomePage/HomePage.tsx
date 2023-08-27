1ecbaed3d9a5ecb0b4090e464885de86499525d3
Yosi7708 <yosi7708@gmail.com>
2023-08-28 01:21:47 +0300

    React-Router 5 Implemented

--- src/layouts/HomePage/HomePage.tsx
+++ src/layouts/HomePage/HomePage.tsx
@@ -3,7 +3,6 @@ import {Carousel} from "./components/Carousel";
 import Heros from "./components/Heros";
 import LibraryServices from "./components/LibraryServices";
 import React from "react";
-
 const HomePage = () => {
     return (
         <>
--- src/layouts/HomePage/components/Carousel.tsx
+++ src/layouts/HomePage/components/Carousel.tsx
@@ -2,6 +2,7 @@ import {useState, useEffect} from 'react';
 import {ReturnBook} from "./ReturnBook";
 import BookModel from "../../../models/BookModel";
 import {SpinnerLoading} from "../../Utils/SpinnerLoading";
+import {Link} from "react-router-dom";
 
 export const Carousel = () => {
 
@@ -104,7 +105,7 @@ export const Carousel = () => {
                 </div>
             </div>
             <div className="homepage-carousel-title mt-3">
-                <a className="btn btn-outline-secondary btn-lg">View More</a>
+                <Link className="btn btn-outline-secondary btn-lg" to="/search">View More</Link>
             </div>
         </div>
     );
