c264ec6ec1d13bcb28e8e47ea7f37dd8719db3e4
Yosi7708 <yosi7708@gmail.com>
2023-08-28 17:13:25 +0300

    CheckoutAndReviewBox Component added

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -2,6 +2,7 @@ import BookModel from "../../models/BookModel";
 import React, {useEffect, useState} from "react";
 import {SpinnerLoading} from "../Utils/SpinnerLoading";
 import {StarsReview} from "../Utils/StarsReview";
+import {CheckoutAndReviewBox} from "./CheckoutAndReviewBox";
 
 export const BookCheckoutPage = () => {
 
@@ -76,6 +77,7 @@ export const BookCheckoutPage = () => {
                             <StarsReview rating={4.5} size={32}/>
                         </div>
                     </div>
+                    <CheckoutAndReviewBox book={book} mobile={false}/>
                 </div>
                 <hr/>
             </div>
@@ -96,6 +98,7 @@ export const BookCheckoutPage = () => {
                         <StarsReview rating={4.5} size={32}/>
                     </div>
                 </div>
+                <CheckoutAndReviewBox book={book} mobile={true}/>
                 <hr/>
             </div>
         </div>
