4f4e7bdaf679f2fcace8fb60d259928e721fa008
Yosi7708 <yosi7708@gmail.com>
2023-09-01 02:22:08 +0300

    implemented Reviews Component ,Model and functionality

---
+++ src/lib/oktaConfig.ts
@@ -0,0 +1,10 @@
+export const oktaConfig = {
+    clientId: '0oab0c3ozjCkS8uhg5d7',
+    issuer: 'https://dev-70715310.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:3000/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+    pkce: true,
+    disableHttpsCheck: true
+
+};
+
