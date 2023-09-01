95b50f9af91017a5aebab3e50c883fbf6811959c
Yosi7708 <yosi7708@gmail.com>
2023-09-01 17:06:29 +0300

    Loans Tab implemented with return and renew function

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,10 @@ import {Redirect, Route, Switch, useHistory} from "react-router-dom";
 import {BookCheckoutPage} from "./layouts/BookCheckoutPage/BookCheckoutPage";
 import {oktaConfig} from "./lib/oktaConfig";
 import {OktaAuth, toRelativeUrl} from "@okta/okta-auth-js";
-import {Security , LoginCallback} from "@okta/okta-react";
+import {Security, LoginCallback, SecureRoute} from "@okta/okta-react";
 import LoginWidget from "./Auth/LoginWidget";
 import {ReviewListPage} from "./layouts/BookCheckoutPage/ReviewListPage/ReviewListPage";
+import {ShelfPage} from "./layouts/ShelfPage/ShelfPage";
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
@@ -52,8 +53,10 @@ export const App = () => {
                             () => <LoginWidget config={oktaConfig}/>
                         }
                         />
-                        <Route path="/login/callback" component={LoginCallback}
-                        />
+                        <Route path="/login/callback" component={LoginCallback}/>
+                        <SecureRoute path='/shelf'>
+                            <ShelfPage/>
+                        </SecureRoute>
 
                     </Switch>
                 </div>
